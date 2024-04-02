1e6c589e3292d4a5694a5a927e167f44af0054c6
SantiJaime <santij03@gmail.com>
2024-04-02 20:23:40 -0300

    añado modales de register y login + esquemas de validacion, modifico banner y navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,19 @@
-
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import NewArrivals from '../components/NewArrivals'
-import Banner from '../components/Banner'
-import Subs from '../components/Subs'
+import React from "react";
+import { Container } from "react-bootstrap";
+import NewArrivals from "../components/NewArrivals";
+import Banner from "../components/Banner";
+import Subs from "../components/Subs";
 
 const HomePage = () => {
   return (
     <>
-  <Banner/>
-<Container fluid>
-<NewArrivals/>
-<Subs/>
-</Container>
+      <Banner />
+      <Container fluid>
+        <NewArrivals />
+        <Subs />
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
